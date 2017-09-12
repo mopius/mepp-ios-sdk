36bbff10600a1dae1bb5c299e81a2eb2b9da3c64
Felix <felix.boxleitner@mopius.com>
2017-09-12 18:50:38 +0200

    updated Examples for new Features

--- Examples/MeppSDK-Demo-Swift/MeppSDK-Demo-Swift/AppDelegate.swift
+++ Examples/MeppSDK-Demo-Swift/MeppSDK-Demo-Swift/AppDelegate.swift
@@ -4,46 +4,62 @@
 //
 //  Created by Patrick Steiner on 22.09.16.
 //  Copyright © 2016 Mopius. All rights reserved.
-//
 
 import UIKit
+import CoreLocation
 import MeppSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-    var meppBeaconManager: MeppBeaconManager?
+    
+    private var deviceStatusManager: MeppDeviceStatusManager?
+    private var meppApiClient: MeppAPIClient?
+    private var sessionManager: MeppSessionManager?
+    private var meppManager: MeppManager?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        MeppSDK.setAppToken("A1B2C3D4", forHost: "example.com") { (successful) in
-            if successful {
-                // init beacon manager
-                self.meppBeaconManager = MeppBeaconManager()
-                self.meppBeaconManager!.delegate = self
-                self.meppBeaconManager!.startMonitoring()
-            }
-        }
+        setupMepp()
         
         return true
     }
+    
+    private func setupMepp() {
+        MeppSDK.setAppToken("A1B2C3D4", forHost: "api.mepp.at", completion: {(successful) in
+            if successful {
+                Log.debug("MEPP SDK setup done")
+                self.meppApiClient = MeppAPIClient()
+                
+                self.meppManager = MeppManager(delegate: self)
+                self.meppManager?.startMonitoring()
+                
+                self.meppApiClient?.nearestPlaces(latitude: 200, longitude: 200, limit: 0, completion: { (success, places, statusCode) in
+                    print(places ?? "")
+                })
+                self.meppApiClient?.notificationDisplayed(contentId: "14")
+                self.meppApiClient?.notificationClicked(contentId: "14")
+            }
+        })
+    }
 }
 
-extension AppDelegate: MeppBeaconManagerDelegate {
-    func didFindNewContent(_ content: Content) {
-        print("Did find content: \(content.contentType)")
+extension AppDelegate : MeppManagerDelegate{
+    func didFindContent(_ content: Content){
+        Log.debug("NEW CONTENT: \(content.description)")
     }
-    
-    func didChangeSessionStatus(_ status: String, critical: Bool) {
-        print("Did change session status: \(status)")
+    func didChangeStatus(_ status: String, critical: Bool){
+        Log.debug("STATUS: \(status)")
     }
-    
-    func didDiscoverBeacons(_ beacons: [DiscoveredBeacon]) {
-        // get discovered beacons
+    func geofencesDidChange(locations: [GeoLocation]){
+        Log.debug("GEOFENCES DID CHANGE")
     }
-    
-    func shouldTrackAnalyticsEvent(_ event: AnalyticsEvent) {
-        // track analytics event
+    func didDiscoverBeacons(_ beacons: [CLBeacon]){
+        // Log.debug("BEACONS DISCOVERED")
+    }
+    func shouldTrackAnalyticsEvent(_ event: AnalyticsEvent){
+        Log.debug("ANALYTICS: \(event.description)")
     }
 }
+
